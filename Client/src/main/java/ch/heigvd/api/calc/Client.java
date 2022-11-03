35ce6745e476f0f1fc691800cca78c12a156acc7
costiiI2 <71190744+costiiI2@users.noreply.github.com>
2022-11-03 15:56:54 +0100

    Update Client.java

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -15,7 +15,7 @@ public class Client {
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
     private static final int BUFFER_SIZE = 1024;
     private static final String IP = "localhost";
-    private static final int PORT = 6969;
+    private static final int PORT = 420;
 
     /**
      * Main function to run client
@@ -24,37 +24,35 @@ public class Client {
      */
 
     public static void main(String[] args) {
-        // Log output on a single line
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+            // Log output on a single line
+                System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        Socket clientSocket = null;
-        try {
-            clientSocket = new Socket(IP, PORT);
-            InputStream fromServer = clientSocket.getInputStream();
-            OutputStream toServer = clientSocket.getOutputStream();
+                Socket clientSocket = null;
+                try {
+                    clientSocket = new Socket(IP, PORT);
+                    InputStream fromServer = clientSocket.getInputStream();
+                    OutputStream toServer = clientSocket.getOutputStream();
 
-            LOG.log(Level.INFO, "Response sent by the server: ");
-            LOG.log(Level.INFO, fromServer.toString());
+                    LOG.log(Level.INFO, "Response sent by the server: ");
+                    LOG.log(Level.INFO, fromServer.toString());
 
-            //write to server from console
-            String input = "";
-            while(true){
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                input = br.readLine();
+                    //write to server from console
+                    String input = "";
+                    while(true){
+                        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                        input = br.readLine();
 
-                if(input.equals("exit"))break;
-                input += "\n";
-                toServer.write(input.getBytes());
-                if(input.endsWith("\n")){
-                    toServer.flush();
+                        if(input.equals("exit"))break;
+                        //TODO le premier caractere est pas lu donc j'ai ajouer un \n
+                        input = "\n" + input + "\n";
+
+                        toServer.write(input.getBytes());
+                        toServer.flush();
+                    }
+                    clientSocket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Error could create a socket form "+IP+" on port "+PORT+ ".\nError:",e);
                 }
-            }
-            clientSocket.close();
-            LOG.log(Level.INFO, "Response sent by the server: ");
-        } catch (IOException e) {
-            throw new RuntimeException("Error could create a socket form "+IP+" on port "+PORT+ ".\nError:",e);
-        }
-
 
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
