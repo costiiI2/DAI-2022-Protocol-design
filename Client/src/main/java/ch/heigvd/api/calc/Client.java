66bbf3b420d8af8af3cebf05335d7a812254d76f
costiiI2 <71190744+costiiI2@users.noreply.github.com>
2022-11-03 11:33:16 +0100

    Update Client.java

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -14,6 +14,7 @@ public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
     private static final int BUFFER_SIZE = 1024;
+    private static final String IP = "localhost";
     private static final int PORT = 6969;
 
     /**
@@ -26,26 +27,32 @@ public class Client {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        final String IP = "";
-        BufferedReader stdin = null;
         Socket clientSocket = null;
-        try( InputStream fromServer = clientSocket.getInputStream();OutputStream toServer = clientSocket.getOutputStream()) {
+        try {
             clientSocket = new Socket(IP, PORT);
-            toServer.write(IP.getBytes());
+            InputStream fromServer = clientSocket.getInputStream();
+            OutputStream toServer = clientSocket.getOutputStream();
 
-            ByteArrayOutputStream responseBuffer = new ByteArrayOutputStream();
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int newBytes;
-            while ((newBytes = fromServer.read(buffer)) != -1) {
-                responseBuffer.write(buffer, 0, newBytes);
-            }
             LOG.log(Level.INFO, "Response sent by the server: ");
-            LOG.log(Level.INFO, responseBuffer.toString());
+            LOG.log(Level.INFO, fromServer.toString());
+            
+            //write to server from console
+            String input = "";
+            while(true){
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                input = br.readLine()+"\n";
+                if(input.equals("exit"))break;
+                toServer.write(input.getBytes());
+                if(input.endsWith("\n")){
+                    toServer.flush();
+                }
+            }
+            clientSocket.close();
+            LOG.log(Level.INFO, "Response sent by the server: ");
         } catch (IOException e) {
             throw new RuntimeException("Error could create a socket form "+IP+" on port "+PORT+ ".\nError:",e);
         }
 
-       
 
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
@@ -56,8 +63,5 @@ public class Client {
          *     - send the command to the server
          *     - read the response line from the server (using BufferedReader.readLine)
          */
-
-        stdin = new BufferedReader(new InputStreamReader(System.in));
-
     }
 }
