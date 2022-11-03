62a662c8d94c64586e7e0dbb323993cf4383988d
costiiI2 <71190744+costiiI2@users.noreply.github.com>
2022-11-03 16:17:45 +0100

    reception message

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -14,6 +14,7 @@ public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
     private static final int BUFFER_SIZE = 1024;
+    private static byte[] buffer = new byte[BUFFER_SIZE];
     private static final String IP = "localhost";
     private static final int PORT = 420;
 
@@ -27,41 +28,34 @@ public class Client {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        Socket clientSocket = null;
         try {
-            clientSocket = new Socket(IP, PORT);
+            Socket clientSocket = new Socket(IP, PORT);
             InputStream fromServer = clientSocket.getInputStream();
             OutputStream toServer = clientSocket.getOutputStream();
 
+            //welcome message from the server
             LOG.log(Level.INFO, "Response sent by the server: ");
-            LOG.log(Level.INFO, fromServer.toString());
+            int read = fromServer.read(buffer);
+            String response = new String(buffer, 0, read);
+            LOG.log(Level.INFO, response);
 
-            //write to server from console
-            String input = "";
             while (true) {
+
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                input = br.readLine();
-
+                String input = br.readLine();
                 if (input.equals("exit")) break;
-                //TODO le premier caractere est pas lu donc j'ai ajouer un \n
-                input = "\n" + input + "\n";
-
+                input = input + "\n";
                 toServer.write(input.getBytes());
                 toServer.flush();
+
+                //get server respones to query
+                read = fromServer.read(buffer);
+                response = new String(buffer, 0, read);
+                LOG.log(Level.INFO, response);
             }
             clientSocket.close();
         } catch (IOException e) {
             throw new RuntimeException("Error could create a socket form " + IP + " on port " + PORT + ".\nError:", e);
         }
-
-        /* TODO: Implement the client here, according to your specification
-         *   The client has to do the following:
-         *   - connect to the server
-         *   - initialize the dialog with the server according to your specification
-         *   - In a loop:
-         *     - read the command from the user on stdin (already created)
-         *     - send the command to the server
-         *     - read the response line from the server (using BufferedReader.readLine)
-         */
     }
 }
